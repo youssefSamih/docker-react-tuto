459135597925390d3e329db6c51d851abf49f08a
Youssef samih <youssefsamih@Youssefs-MacBook-Pro.local>
2022-02-11 09:30:51 +0100

    feat: change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn AWS deployment
         </a>
       </header>
     </div>
